f0e014af26753d8b5f3da5065f151974a9fd95df
TechnoDogs-3707 <60295775+TechnoDogs-3707@users.noreply.github.com>
2021-01-24 12:05:10 -0500

    Added a few more examples

--- Scout/src/main/java/frc/robot/Robot.java
+++ Scout/src/main/java/frc/robot/Robot.java
@@ -6,19 +6,24 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with tank
  * steering and an Xbox controller.
  */
 public class Robot extends TimedRobot {
+  private static final int kPDPId = 0;
+
   private final PWMVictorSPX m_leftMotor = new PWMVictorSPX(0);
   private final PWMVictorSPX m_rightMotor = new PWMVictorSPX(1);
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
   private final XboxController m_driverController = new XboxController(0);
+  private final PowerDistributionPanel m_pdp = new PowerDistributionPanel(kPDPId);
 
   @Override
   public void teleopInit() {
@@ -36,5 +41,24 @@ public class Robot extends TimedRobot {
     // moves the right side of the robot forward and backward.
     m_robotDrive.tankDrive(
         m_driverController.getY(Hand.kLeft), m_driverController.getY(Hand.kRight));
+
+
+    /*
+     * Get the current going through channel 7, in Amperes. The PDP returns the
+     * current in increments of 0.125A. At low currents
+     * the current readings tend to be less accurate.
+     */
+    SmartDashboard.putNumber("Current Channel 7", m_pdp.getCurrent(7));
+
+    /*
+     * Get the voltage going into the PDP, in Volts.
+     * The PDP returns the voltage in increments of 0.05 Volts.
+     */
+    SmartDashboard.putNumber("Voltage", m_pdp.getVoltage());
+
+    /*
+     * Retrieves the temperature of the PDP, in degrees Celsius.
+     */
+    SmartDashboard.putNumber("Temperature", m_pdp.getTemperature());
   }
 }
