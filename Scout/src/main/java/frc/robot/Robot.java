a7427f44acc89a83fe5d4ea6018b067689163fde
TechnoDogs-3707 <60295775+TechnoDogs-3707@users.noreply.github.com>
2021-01-24 12:38:22 -0500

    added more PDP logging

--- Scout/src/main/java/frc/robot/Robot.java
+++ Scout/src/main/java/frc/robot/Robot.java
@@ -25,12 +25,26 @@ public class Robot extends TimedRobot {
   private final XboxController m_driverController = new XboxController(0);
   private final PowerDistributionPanel m_pdp = new PowerDistributionPanel(kPDPId);
 
+  Double max_current_2 = 0.0;
+  Double max_current_3 = 0.0;
+  Double min_voltage = 0.0;
+
+  Double max_total_current = 0.0;
+  Double max_total_power = 0.0;
+
   @Override
   public void teleopInit() {
     // TODO Auto-generated method stub
     super.teleopInit();
     m_leftMotor.setInverted(true);
     m_rightMotor.setInverted(true);
+
+    max_current_2 = 0.0;
+    max_current_3 = 0.0;
+    min_voltage = 24.0;
+
+    max_total_current = 0.0;
+    max_total_power = 0.0;
   }
 
   @Override
@@ -48,13 +62,61 @@ public class Robot extends TimedRobot {
      * current in increments of 0.125A. At low currents
      * the current readings tend to be less accurate.
      */
-    SmartDashboard.putNumber("Current Channel 7", m_pdp.getCurrent(7));
+
+     Double current_current_2 = m_pdp.getCurrent(2);
+     Double current_current_3 = m_pdp.getCurrent(3);
+
+     if (current_current_2 > max_current_2) {
+       max_current_2 = current_current_2;
+     }
+
+     if (current_current_3 > max_current_3) {
+       max_current_3 = current_current_3;
+     }
+
+
+    SmartDashboard.putNumber("Current Channel 2", current_current_2);
+    SmartDashboard.putNumber("Current Channel 3", current_current_3);
+
+    SmartDashboard.putNumber("Max Current Channel 2", max_current_2);
+    SmartDashboard.putNumber("Max Current Channel 3", max_current_3);
 
     /*
      * Get the voltage going into the PDP, in Volts.
      * The PDP returns the voltage in increments of 0.05 Volts.
      */
-    SmartDashboard.putNumber("Voltage", m_pdp.getVoltage());
+
+    Double current_voltage = m_pdp.getVoltage();
+
+    Double current_total_current = m_pdp.getTotalCurrent();
+    Double current_total_power = m_pdp.getTotalPower();
+
+    if (current_total_current > max_total_current){
+      max_total_current = current_total_current;
+    }
+
+    if (current_total_power > current_total_power){
+      max_total_power = current_total_power;
+    }
+
+    Double total_energy = m_pdp.getTotalEnergy();
+
+    SmartDashboard.putNumber("Total Current (max)", max_total_current);
+    SmartDashboard.putNumber("Total Current", current_total_current);
+
+    SmartDashboard.putNumber("Total Power (max)", max_total_power);
+    SmartDashboard.putNumber("Total Power", current_total_power);
+
+    SmartDashboard.putNumber("Total Energy", total_energy);
+
+    
+
+    if (current_voltage < min_voltage){
+      min_voltage = current_voltage;
+    }
+
+    SmartDashboard.putNumber("Voltage", current_voltage);
+    SmartDashboard.putNumber("Min Voltage", min_voltage);
 
     /*
      * Retrieves the temperature of the PDP, in degrees Celsius.
